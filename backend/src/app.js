6281fae29ab123ea6418836c12ed69511a9030d9
ChaitanyaPuliga <chaitanyapuliga@gmail.com>
2026-07-21 14:15:29 +0530

    server.js and app.js files created.

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to ServiceHub API"
+    });
+});
+
+module.exports = app;
